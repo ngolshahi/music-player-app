3d3e831ab107a62565dd45c3b925d51d47910ae0
ng7g22 <ng7g22@soton.ac.uk>
2025-03-16 13:10:08 +0000

    Project setup

---
+++ music-player/eslint.config.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      ignores: [
+        "**/node_modules/**",
+        "**/.expo/**",
+        "**/.next/**",
+        "**/__generated__/**",
+        "**/build/**",
+        "/react-native-lab/react-native/**",
+        "/docs/react-native-website/**",
+        "**/android/**",
+        "**/assets/**",
+        "**/bin/**",
+        "**/fastlane/**",
+        "**/ios/**",
+        "**/kotlin/providers/**",
+        "**/vendored/**",
+        "/docs/public/static/**"
+      ]
+    }
+  ];
+  
