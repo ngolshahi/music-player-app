cf0afc12d89d20d17405db0636de541cb90d46ff
ng7g22 <ng7g22@soton.ac.uk>
2025-03-16 13:40:13 +0000

    Added dark style to app

--- music-player/src/app/(tabs)/_layout.tsx
+++ music-player/src/app/(tabs)/_layout.tsx
@@ -1,8 +1,42 @@
 import { Tabs } from 'expo-router'
+import { colors, fontSize } from '@/constants/tokens'
+import { BlurView } from 'expo-blur'
+import { StyleSheet } from 'react-native'
+
+
 
 const TabsNavigation = () => {
     return (
-        <Tabs>
+        <Tabs 
+            screenOptions={{
+                tabBarActiveTintColor: colors.primary,
+                tabBarLabelStyle: {
+                    fontSize: fontSize.xs,
+                    fontWeight: '500',
+                },
+                headerShown: false,
+                tabBarStyle: {
+                    position: 'absolute',
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    borderTopWidth: 0,
+                    paddingTop: 0
+                },
+                tabBarBackground: () => (
+                    <BlurView intensity={95} 
+                        style={{
+                            ...StyleSheet.absoluteFillObject,
+                            overflow: 'hidden',
+                            borderTopLeftRadius: 20,
+                            borderTopRightRadius: 20,
+
+                        }}
+                    />
+                )
+
+
+            }}
+        > 
             <Tabs.Screen name="favourites"/>
             <Tabs.Screen name="playlists"/>
             <Tabs.Screen name="(songs)"/>
