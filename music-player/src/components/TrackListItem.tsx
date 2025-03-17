33d50d43ff27328f43ff45e14aec0bd2b6effe9a
ng7g22 <golshahi@icloud.com>
2025-03-17 20:41:24 +0000

    Added event handler for track selection

--- music-player/src/components/TrackListItem.tsx
+++ music-player/src/components/TrackListItem.tsx
@@ -9,12 +9,14 @@ import { Entypo, Ionicons } from '@expo/vector-icons';
 
 export type TrackListItemProps = {
   track: Track
+  onTrackSelect: (track: Track) => void
 }
 
-export const TrackListItem = ({ track }: TrackListItemProps) => {
+export const TrackListItem = ({ track, onTrackSelect: handleTrackSelect }: TrackListItemProps) => {
   const isActiveTrack = useActiveTrack()?.url === track.url
   return (
     <TouchableHighlight
+      onPress= {() => handleTrackSelect(track)}
       underlayColor={colors.background}
       style={styles.touchable}
     >
