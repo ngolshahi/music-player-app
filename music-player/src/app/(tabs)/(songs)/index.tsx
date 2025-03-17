2dc8247524809f32d5b88dcd45b8547292a400f2
ng7g22 <golshahi@icloud.com>
2025-03-17 20:02:11 +0000

    Adding search functionality

--- music-player/src/app/(tabs)/(songs)/index.tsx
+++ music-player/src/app/(tabs)/(songs)/index.tsx
@@ -2,12 +2,23 @@ import React from 'react';
 import { View, ScrollView } from 'react-native';
 import { TracksList } from "@/components/TracksList";
 import { defaultStyles } from "@/styles";
+import { screenPadding } from '@/constants/tokens';
+import { useNavigationSearch } from '@/hooks/useNavigationSearch';
 
 
 const SongsScreen = () => {
+    const search = useNavigationSearch({
+        searchBarOptions: {
+            placeholder: 'Find in songs',
+        },    
+    })
+    
     return (
         <View style={defaultStyles.container}>
-           <ScrollView>
+           <ScrollView
+            contentInsetAdjustmentBehavior='automatic'
+            style={{paddingHorizontal: screenPadding.horizontal}}
+           >
                 <TracksList scrollEnabled={false}/>
            </ScrollView>
         </View>
