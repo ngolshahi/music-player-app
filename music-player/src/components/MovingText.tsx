f1cf6de8d7e3718e772e7b8958a8941a323a3d87
ng7g22 <golshahi@icloud.com>
2025-03-21 17:32:41 +0000

    Created moving text component

--- music-player/src/components/MovingText.tsx
+++ music-player/src/components/MovingText.tsx
@@ -1,11 +1,57 @@
-import {StyleProps} from 'react-native-reanimated'
+import { useEffect } from 'react'
+import { StyleProp, TextStyle } from 'react-native'
+import Animated, {cancelAnimation, Easing, StyleProps, useAnimatedStyle, useSharedValue, withDelay, withRepeat, withTiming} from 'react-native-reanimated'
 
 export type MovingTextProps = {
     text: string
     animationThreshold: number
-    style?: StyleProps
+    style?: StyleProp<TextStyle>
 }
 
-export const MovingText = () => {
+export const MovingText = ( {text, animationThreshold, style}: MovingTextProps) => {
+    const translateX = useSharedValue(0)
+    const shouldAnimate = text.length >= animationThreshold
+
+    const textWidth = text.length * 3
+
+    useEffect(() => {
+        if (!shouldAnimate) return
+
+        translateX.value = withDelay(1000, withRepeat(withTiming(-textWidth, {
+            duration: 5000,
+            easing: Easing.linear,
+        }), -1, true),)
+
+        return () => {
+            cancelAnimation(translateX)
+            translateX.value = 0
+        }
+
+    }, [translateX, text, animationThreshold, shouldAnimate, textWidth])
+            
+
+    const animatedStyle = useAnimatedStyle(() => {
+        return {
+            transform: [{translateX: translateX.value}],
+        }
+    })
+
+    
+    
+    return (
+        <Animated.Text 
+            numberOfLines={1} 
+            style={[
+                style,
+                animatedStyle,
+                shouldAnimate && {
+                    width: 9999, // Preventing the ellipsis from appearing
+                    paddingLeft: 16, //Avoid initial characters being barely visible
+                },
+            ]}
+        > 
+            {text} 
+        </Animated.Text>
+    )
 
 }
