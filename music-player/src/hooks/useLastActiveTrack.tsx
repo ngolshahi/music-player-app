a05135c06b3b3d507a7bf9f995c6c79276514562
ng7g22 <golshahi@icloud.com>
2025-03-21 16:07:04 +0000

    Fixed issue with no () after function

--- music-player/src/hooks/useLastActiveTrack.tsx
+++ music-player/src/hooks/useLastActiveTrack.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { Track, useActiveTrack } from "react-native-track-player"
 
 export const useLastActiveTrack = () => {
-    const activeTrack = useActiveTrack
+    const activeTrack = useActiveTrack()
     const [lastActiveTrack, setLastActiveTrack] = useState<Track>()
 
     useEffect(() => {
