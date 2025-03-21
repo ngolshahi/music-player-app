da732772fcbee2aab1aac52c68be20b39e45cb14
ng7g22 <golshahi@icloud.com>
2025-03-21 17:14:30 +0000

    Added style to properties of moving text

--- music-player/src/components/MovingText.tsx
+++ music-player/src/components/MovingText.tsx
@@ -1,7 +1,9 @@
+import {StyleProps} from 'react-native-reanimated'
 
 export type MovingTextProps = {
     text: string
     animationThreshold: number
+    style?: StyleProps
 }
 
 export const MovingText = () => {
