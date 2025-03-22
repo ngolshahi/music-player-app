1c288baba09db65630d99417338b9c6ea85bc171
ng7g22 <golshahi@icloud.com>
2025-03-22 23:25:57 +0000

    Added Dismiss Player Symbol

---
+++ music-player/src/app/player.tsx
@@ -0,0 +1,37 @@
+import { screenPadding } from "@/constants/tokens"
+import { defaultStyles } from "@/styles"
+import { View, StyleSheet } from "react-native"
+import { useSafeAreaInsets } from "react-native-safe-area-context"
+
+const PlayerScreen = () => {
+    return <View style={styles.overlayContainer} >
+
+        <DismissPlayerSymbol />
+        </View>
+}
+
+const DismissPlayerSymbol = () => {
+    const {top} = useSafeAreaInsets()
+
+    return <View style ={{
+        position: 'absolute',
+        top: top + 8,
+        left: 0,
+        right: 0,
+        flexDirection: 'row',
+        justifyContent: 'center'
+    }}>
+
+        
+    </View>
+}
+
+const styles = StyleSheet.create({
+    overlayContainer: {
+        ...defaultStyles.container,
+        paddingHorizontal: screenPadding.horizontal,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+    },
+})
+
+export default PlayerScreen
