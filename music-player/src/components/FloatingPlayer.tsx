392cbd37ba6e2c942c06d3e79b45793ed421c0f1
ng7g22 <golshahi@icloud.com>
2025-03-22 23:31:38 +0000

    Added function for handling press on floating player

--- music-player/src/components/FloatingPlayer.tsx
+++ music-player/src/components/FloatingPlayer.tsx
@@ -8,17 +8,21 @@ import { Track, useActiveTrack } from "react-native-track-player"
 import { PlayPauseButton, SkipToNextButton } from "./PlayerControls"
 import { useLastActiveTrack } from "@/hooks/useLastActiveTrack"
 import { MovingText } from "./MovingText"
+import { useRouter } from "expo-router"
 
 export const FloatingPlayer = ({style} : ViewProps) => {
+    const route = useRouter()
     const activeTrack = useActiveTrack()
     const lastActiveTrack = useLastActiveTrack()
 
     const displayedTrack = activeTrack ?? lastActiveTrack
 
+    const 
+
     if (!displayedTrack) return null
 
 
-    return <TouchableOpacity activeOpacity={0.9} style={[styles.container, style]}>
+    return <TouchableOpacity onPress={handlePress} activeOpacity={0.9} style={[styles.container, style]}>
         <>
             <FastImage source={{
                 uri: displayedTrack.artwork ?? unknownTrackImageUri
