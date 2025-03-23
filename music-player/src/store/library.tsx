ecc90086b8261ca4d97a9f9a013c54d1302ab4af
ng7g22 <golshahi@icloud.com>
2025-03-23 22:54:32 +0000

    Imports

--- music-player/src/store/library.tsx
+++ music-player/src/store/library.tsx
@@ -1,7 +1,9 @@
 import { Track } from "react-native-track-player"
+import library from '@/assets/data/library.json'
+import { Artist, Playlist, TrackWithPlaylist } from '@/helpers/types'
 
 interface LibraryState {
-    tracks: TrackWithPlayList[]
+    tracks: TrackWithPlaylist[]
     toggleTrackFavorite: (track: Track) => void
     addToPlaylist: (track: Track, playlistName: string) => void
 }
