3f92d8565981f459a54a5409e06b9d2f8f210505
ng7g22 <golshahi@icloud.com>
2025-03-23 22:49:40 +0000

    Added function for checking if a song is a favorite

--- music-player/src/hooks/useTrackPlayerFavorite.tsx
+++ music-player/src/hooks/useTrackPlayerFavorite.tsx
@@ -1,3 +1,11 @@
+import { useActiveTrack } from "react-native-track-player"
+
 export const useTrackPlayerFavorite = () => {
+    const activeTrack = useActiveTrack()
+
+    const { favorites, toggleTrackFavorite } = useFavorites()
+
+    const isFavorite = favorites.find((track) => track.url === activeTrack?.uril)?.rating === 1
+
     
 }
