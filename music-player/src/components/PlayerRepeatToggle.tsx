145884e54f36c8ef18d0dc1bc0d489a87c938f63
ng7g22 <golshahi@icloud.com>
2025-03-26 13:59:36 +0000

    Implemented toggleRepeatMode function

--- music-player/src/components/PlayerRepeatToggle.tsx
+++ music-player/src/components/PlayerRepeatToggle.tsx
@@ -2,6 +2,8 @@ import { RepeatMode } from "react-native-track-player"
 import { match } from "ts-pattern"
 import { ComponentProps } from "react"
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { useTrackPlayerRepeatMode } from "@/hooks/useTrackPlayerRepeatMode"
+import { colors } from "@/constants/tokens"
 
 
 type IconProps = Omit<ComponentProps<typeof MaterialCommunityIcons>, 'name'>
@@ -15,13 +17,23 @@ const repeatOrder = [
 ] as const
 
 export const PlayerRepeatToggle = ({...iconProps} : IconProps) => {
-    const repeatMode = useTrackPlayerRepeatMode()
+    const {repeatMode, changeRepeatMode} = useTrackPlayerRepeatMode()
+
+    const toggleRepeatMode = () => {
+        if (repeatMode == null) return
+
+        const currentIndex = repeatOrder.indexOf(repeatMode)
+
+        const nextIndex = (currentIndex + 1) % repeatOrder.length
+
+        changeRepeatMode(repeatOrder[nextIndex])
+    }
 
     const icon = match(repeatMode).returnType<IconName>()
     .with(RepeatMode.Off, () => 'repeat-off')
     .with(RepeatMode.Track, () => 'repeat-once')
     .with(RepeatMode.Queue, () => 'repeat')
-    .otherwise('repeat-off')
+    .otherwise(() => 'repeat-off')
 
     return <MaterialCommunityIcons name={icon} onPress={toggleRepeatMode} color={colors.icon} {...iconProps} />
 
