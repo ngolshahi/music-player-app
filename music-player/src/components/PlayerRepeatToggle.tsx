d99eec32243ccef271545a54bec4614bd50b9ec5
ng7g22 <golshahi@icloud.com>
2025-03-26 13:50:07 +0000

    Added icon pattern matching

--- music-player/src/components/PlayerRepeatToggle.tsx
+++ music-player/src/components/PlayerRepeatToggle.tsx
@@ -1,4 +1,10 @@
 import { RepeatMode } from "react-native-track-player"
+import { match } from "ts-pattern"
+import { ComponentProps } from "react"
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+
+type IconName = ComponentProps<typeof MaterialCommunityIcons>['name']
 
 const repeatOrder = [
     RepeatMode.Off,
@@ -7,5 +13,7 @@ const repeatOrder = [
 ] as const
 
 export const PlayerRepeatToggle = () => {
-    
+    const repeatMode = RepeatMode.Off
+
+    const icon = match(repeatMode).returnType<IconName>()
 }
