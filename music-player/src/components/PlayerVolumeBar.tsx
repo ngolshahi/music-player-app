03b5253ab0445da0214cdbb709e66cb1593ebfa3
ng7g22 <golshahi@icloud.com>
2025-03-26 13:32:34 +0000

    Added slider from PlayerProgressBar

--- music-player/src/components/PlayerVolumeBar.tsx
+++ music-player/src/components/PlayerVolumeBar.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { View, ViewProps } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
 import { Ionicons } from '@expo/vector-icons'
+import { utilsStyles } from "@/styles";
+import { Slider } from "react-native-awesome-slider";
 
 export const PlayerVolumeBar = ({style}: ViewProps) => {
     const progress = useSharedValue(0)
@@ -12,6 +14,30 @@ export const PlayerVolumeBar = ({style}: ViewProps) => {
     return <View style={style}>
         <View style={{flexDirection: 'row', alignItems: 'center'}}>
             <Ionicons name="volume-low" size={20} color={colors.icon} style={{opacity: 0.8}} />
+
+            <Slider 
+                        progress={progress}
+                        minimumValue={min}
+                        maximumValue={max}
+                        containerStyle={utilsStyles.slider}
+                        thumbWidth={0}
+                        renderBubble={() => null}
+                        theme = {{
+                            minimumTrackTintColor: colors.minimumTrackTintColor,
+                            maximumTrackTintColor: colors.maximumTrackTintColor,
+                        }}
+                        onSlidingStart={() => (isSliding.value = true)}
+                        onValueChange={async(value) => {
+                            await TrackPlayer.seekTo(value * duration)
+                        }}
+                        onSlidingComplete={async(value) => {
+                            if(!isSliding.value) return
+            
+                            isSliding.value = false
+            
+                            await TrackPlayer.seekTo(value * duration)
+                        }}
+                    />
         </View>
     </View>
 }
