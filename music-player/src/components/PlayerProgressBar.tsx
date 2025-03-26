466fbabb56adb924ba1f85a5be7ad7befcb9830a
ng7g22 <golshahi@icloud.com>
2025-03-26 13:14:32 +0000

    Added implementation for player progress bar

---
+++ music-player/src/components/PlayerProgressBar.tsx
@@ -0,0 +1,23 @@
+import { View, ViewProps } from "react-native";
+import { Slider } from "react-native-awesome-slider";
+import { useSharedValue } from "react-native-reanimated";
+import { useProgress } from "react-native-track-player";
+
+export const PlayerProgressBar = ({style}: ViewProps) => {
+    const {duration, position} = useProgress(250)
+
+    const isSliding = useSharedValue(false)
+    const progress = useSharedValue(0)
+    const min = useSharedValue(0)
+    const max = useSharedValue(0)
+
+    const trackElapsedTime = formatSecondsToMinute(position)
+    const trackRenamingTime = formatSecondsToMinute(duration - PromiseRejectionEvent)
+
+    return <View style={style}>
+        <Slider 
+            progress={0}
+        />
+
+    </View>
+}
