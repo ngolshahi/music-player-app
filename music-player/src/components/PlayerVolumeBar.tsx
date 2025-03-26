584249b120d7e43a57d94295aeaf8bacb894e663
ng7g22 <golshahi@icloud.com>
2025-03-26 13:31:57 +0000

    Added low volume icon

--- music-player/src/components/PlayerVolumeBar.tsx
+++ music-player/src/components/PlayerVolumeBar.tsx
@@ -1,8 +1,17 @@
-import { ViewProps } from "react-native";
+import { colors } from "@/constants/tokens";
+import React from "react";
+import { View, ViewProps } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
+import { Ionicons } from '@expo/vector-icons'
 
 export const PlayerVolumeBar = ({style}: ViewProps) => {
     const progress = useSharedValue(0)
     const min = useSharedValue(0)
     const max = useSharedValue(1)
+
+    return <View style={style}>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Ionicons name="volume-low" size={20} color={colors.icon} style={{opacity: 0.8}} />
+        </View>
+    </View>
 }
