cc81637ac34355052b6eee346f7ade808c99fb6c
ng7g22 <golshahi@icloud.com>
2025-03-26 13:47:21 +0000

    Added different states of the repeat toggle

--- music-player/src/components/PlayerRepeatToggle.tsx
+++ music-player/src/components/PlayerRepeatToggle.tsx
@@ -1,3 +1,11 @@
+import { RepeatMode } from "react-native-track-player"
+
+const repeatOrder = [
+    RepeatMode.Off,
+    RepeatMode.Track,
+    RepeatMode.Queue,
+]
+
 export const PlayerRepeatToggle = () => {
-    
+
 }
