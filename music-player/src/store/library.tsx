6c214a4a1097d7b273b06ecc448adb170db84b8b
ng7g22 <golshahi@icloud.com>
2025-03-26 14:05:53 +0000

    Added favorites and artists

--- music-player/src/store/library.tsx
+++ music-player/src/store/library.tsx
@@ -1,9 +1,71 @@
-import { Track } from "react-native-track-player"
 import library from '@/assets/data/library.json'
+import { unknownTrackImageUri } from '@/constants/images'
 import { Artist, Playlist, TrackWithPlaylist } from '@/helpers/types'
+import { Track } from 'react-native-track-player'
+import { create } from 'zustand'
 
 interface LibraryState {
-    tracks: TrackWithPlaylist[]
-    toggleTrackFavorite: (track: Track) => void
-    addToPlaylist: (track: Track, playlistName: string) => void
+	tracks: TrackWithPlaylist[]
+	toggleTrackFavorite: (track: Track) => void
+	addToPlaylist: (track: Track, playlistName: string) => void
 }
+
+export const useLibraryStore = create<LibraryState>()((set) => ({
+	tracks: library,
+	toggleTrackFavorite: (track) =>
+		set((state) => ({
+			tracks: state.tracks.map((currentTrack) => {
+				if (currentTrack.url === track.url) {
+					return {
+						...currentTrack,
+						rating: currentTrack.rating === 1 ? 0 : 1,
+					}
+				}
+
+				return currentTrack
+			}),
+		})),
+	addToPlaylist: (track, playlistName) =>
+		set((state) => ({
+			tracks: state.tracks.map((currentTrack) => {
+				if (currentTrack.url === track.url) {
+					return {
+						...currentTrack,
+						playlist: [...(currentTrack.playlist ?? []), playlistName],
+					}
+				}
+
+				return currentTrack
+			}),
+		})),
+}))
+
+export const useTracks = () => useLibraryStore((state) => state.tracks)
+
+export const useFavorites = () => {
+	const favorites = useLibraryStore((state) => state.tracks.filter((track) => track.rating === 1))
+	const toggleTrackFavorite = useLibraryStore((state) => state.toggleTrackFavorite)
+
+	return {
+		favorites,
+		toggleTrackFavorite,
+	}
+}
+
+export const useArtists = () =>
+	useLibraryStore((state) => {
+		return state.tracks.reduce((acc, track) => {
+			const existingArtist = acc.find((artist) => artist.name === track.artist)
+
+			if (existingArtist) {
+				existingArtist.tracks.push(track)
+			} else {
+				acc.push({
+					name: track.artist ?? 'Unknown',
+					tracks: [track],
+				})
+			}
+
+			return acc
+		}, [] as Artist[])
+	})
