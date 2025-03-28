9abf5efa4aaf86b8231ba71783d1992521f8da62
ng7g22 <golshahi@icloud.com>
2025-03-28 23:08:44 +0000

    Changed values

--- music-player/src/components/PlayerProgressBar.tsx
+++ music-player/src/components/PlayerProgressBar.tsx
@@ -6,13 +6,13 @@ import { Slider } from 'react-native-awesome-slider'
 import { useSharedValue } from 'react-native-reanimated'
 import TrackPlayer, { useProgress } from 'react-native-track-player'
 
-export const PlayerProgressBar = ({style}: ViewProps) => {
-    const {duration, position} = useProgress(250)
-
-    const isSliding = useSharedValue(false)
-    const progress = useSharedValue(0)
-    const min = useSharedValue(0)
-    const max = useSharedValue(1)
+export const PlayerProgressBar = ({ style }: ViewProps) => {
+	const { position } = useProgress(250)
+	const duration = 300
+	const isSliding = useSharedValue(false)
+	const progress = useSharedValue(0)
+	const min = useSharedValue(0)
+	const max = useSharedValue(1)
 
     const trackElapsedTime = formatSecondsToMinutes(position)
     const trackRenamingTime = formatSecondsToMinutes(duration - position)
